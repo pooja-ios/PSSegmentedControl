3a6b7c996148901506204c75679ab54e69cd0256
pooja-ios <poojasharma.net@gmail.com>
2020-03-06 11:21:33 +0000

    Initial Commit

---
+++ PSSegmentedControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PSSegmentedControl
+//
+//  Created by pooja sharma on 06/03/20.
+//  Copyright © 2020 pooja sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
