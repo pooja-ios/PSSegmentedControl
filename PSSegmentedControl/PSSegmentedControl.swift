6b1a63c88bfb579fd7d50c9eb5cfdf15118bb39f
pooja-ios <poojasharma.net@gmail.com>
2020-05-04 14:52:37 +0100

    first commit

---
+++ PSSegmentedControl/PSSegmentedControl.swift
@@ -0,0 +1,21 @@
+//
+//  PSSegmentedControl.swift
+//  PSSegmentedControl
+//
+//  Created by pooja sharma on 06/03/20.
+//  Copyright © 2020 pooja sharma. All rights reserved.
+//
+
+import UIKit
+
+class PSSegmentedControl: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
